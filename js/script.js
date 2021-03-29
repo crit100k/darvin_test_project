5c923163c867841a149e31f5dfccdd433e78c873
alex_grigorovich <crit1k8912@gmail.com>
2021-03-29 12:40:01 +0300

    added dropdown menu

--- js/script.js
+++ js/script.js
@@ -10,6 +10,7 @@ $(document).ready(function(){
         autoplay: false,
         autoplayTimeout: 5000,
         dots:false,
+        smartSpeed:1000
         // nav: true,
         // navText:["",""],
         // navClass: ["slider__arrow-left","slider__arrow-right"],
@@ -21,6 +22,41 @@ $(document).ready(function(){
   });
     prev.on("click", ()=>{
         $('.owl-carousel').trigger("prev.owl.carousel");
-  })
+  });
+
+  function validateForms(form) {
+	$(form).validate({
+		rules: {
+			name:"required",
+			phone:"required"
+		},
+		messages: {
+			name: "Пожалуйста, введите свое имя",
+			phone: "Пожалуйста, введите свой номер телефона"
+		  	}
+	  	});	
+  	};
+
+	validateForms('form#feed');
+
+
+  $("input[name=phone]").mask("+7 (999) 999-99-99",{placeholder:"_"});
+
+
+//    отправка на сервер
+	// $("form").submit(function(e) {
+	// 	e.preventDefault();
+	// 	$.ajax ({
+	// 		type: "POST",
+	// 		url: "mailer/smart.php",
+	// 		data: $(this).serialize()
+	// 	}).done(function () {
+	// 		$(this).find("input").val("");
+
+	// 		$('form').trigger('reset');
+	// 	});
+		
+	// 	return false;
+	// });  
 
 })          
